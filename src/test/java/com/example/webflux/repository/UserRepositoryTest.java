bbfdc49daa0dc6c02871254e1785eb16729e1199
ls-rain <swlee19990115@gmail.com>
2024-05-29 16:07:50 +0900

    Spring Webflux로 Restful API구현 및 Controller기능 Test 추가

--- src/test/java/com/example/webflux/repository/UserRepositoryTest.java
+++ src/test/java/com/example/webflux/repository/UserRepositoryTest.java
@@ -1,24 +1,55 @@
 package com.example.webflux.repository;
 
 import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRepositoryTest {
+    private final UserRepository userRepository = new UserRepositoryImpl();
 
     @Test
     void save() {
+        User user = User.builder().name("uni1").email("uni1@gmail.com").build();
+        StepVerifier.create(userRepository.save(user))
+                .assertNext(u -> {
+                    assertEquals(1L, u.getId());
+                    assertEquals("uni1", u.getName());
+                    assertEquals("uni1@gmail.com", u.getEmail());
+                })
+                .verifyComplete();
     }
 
     @Test
     void findAll() {
+        userRepository.save(User.builder().name("uni1").email("uni1@gmail.com").build());
+        userRepository.save(User.builder().name("uni2").email("uni2@gmail.com").build());
+        userRepository.save(User.builder().name("uni3").email("uni3@gmail.com").build());
+        StepVerifier.create(userRepository.findAll())
+                .expectNextCount(3)
+                .verifyComplete();
     }
 
     @Test
     void findById() {
+        userRepository.save(User.builder().name("uni1").email("uni1@gmail.com").build());
+        userRepository.save(User.builder().name("uni2").email("uni2@gmail.com").build());
+
+        StepVerifier.create(userRepository.findById(2L))
+                .assertNext(u -> {
+                    assertEquals(2L, u.getId());
+                    assertEquals("uni2", u.getName());
+                })
+                .verifyComplete();
     }
 
     @Test
     void deleteById() {
+        userRepository.save(User.builder().name("uni1").email("uni1@gmail.com").build());
+        userRepository.save(User.builder().name("uni2").email("uni2@gmail.com").build());
+
+        StepVerifier.create(userRepository.deleteById(2L))
+                .expectNextCount(1)
+                .verifyComplete();
     }
 }
